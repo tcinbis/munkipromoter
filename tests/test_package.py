02324ee53c074b6670c3203e8fad50761f483621
Tom Cinbis <tom.cinbis@unibas.ch>
2019-05-20 11:46:24 +0200

    Extended comparision test

--- tests/test_package.py
+++ tests/test_package.py
@@ -9,7 +9,7 @@ def test_package_equality():
         "Firefox",
         "61.2.3",
         "testing",
-        datetime.strptime("10:05:55 01.01.2020","%H:%M:%S %d.%m.%Y"),
+        datetime.strptime("10:05:55 01.01.2020", "%H:%M:%S %d.%m.%Y"),
         False,
         False,
         False,
@@ -22,7 +22,7 @@ def test_package_equality():
         "Firefox",
         "61.2.3",
         "testing",
-        datetime.strptime("10:05:55 01.01.2020","%H:%M:%S %d.%m.%Y"),
+        datetime.strptime("10:05:55 01.01.2020", "%H:%M:%S %d.%m.%Y"),
         False,
         False,
         False,
@@ -64,5 +64,68 @@ def test_package_inequality():
 
     assert p1 != p2
 
-def test_package_less_than():
-    pass
+
+def test_package_version_comparision():
+    """Test the version comparision methods of the package class."""
+    p1 = Package(
+        "Firefox",
+        "61.2.1",
+        "testing",
+        datetime.now(),
+        False,
+        False,
+        False,
+        JiraBoardProvider,
+        "testing",
+        "present",
+    )
+
+    p2 = Package(
+        "Firefox",
+        "61.2.3",
+        "testing",
+        datetime.now(),
+        False,
+        False,
+        False,
+        JiraBoardProvider,
+        "testing",
+        "present",
+    )
+
+    p3 = Package(
+        "Firefox",
+        "61.2.1a",
+        "testing",
+        datetime.now(),
+        False,
+        False,
+        False,
+        JiraBoardProvider,
+        "testing",
+        "present",
+    )
+
+    p4 = Package(
+        "Firefox",
+        "61.3.1a",
+        "testing",
+        datetime.now(),
+        False,
+        False,
+        False,
+        JiraBoardProvider,
+        "testing",
+        "present",
+    )
+
+    assert p1 < p2
+    assert p1 < p3
+    assert p3 < p2
+    assert p1 == p1
+    assert p3 > p1
+    assert p3 >= p1
+    assert p4 > p1
+    assert p4 > p2
+    assert p4 > p3
+    assert p4 >= p4
