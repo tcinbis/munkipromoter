8237718ba61bacb28e022d9e27ec8dfcda83c7eb
Tom Cinbis <tom.cinbis@unibas.ch>
2019-05-27 12:41:02 +0200

    Updated load and get methods for Provider class and added documentation.

--- src/core/base_classes.py
+++ src/core/base_classes.py
@@ -3,24 +3,37 @@ from datetime import datetime
 from distutils.version import LooseVersion
 import utils.config
 from utils.config import PackageState
+from utils import logger as l
+
+logger = l.get_logger(__file__)
 
 
 class Provider:
     def __init__(self, name):
         self.name = name
+        self.is_loaded = False
+        self._packages = list()
 
-    def connect(self) -> bool:
+    def connect(self) -> "bool":
         """
         Check whether a connection is already established or try to establish a new one.
         :return: True if the connection was already established or a new one could be created. Otherwise False
         """
         pass
 
-    def load(self):
+    def load(self) -> "None":
         pass
 
-    def get(self):
-        pass
+    def get(self) -> "[Package]":
+        """
+        If the providers load method was already called this method will return all received packages in a list.
+        Otherwise it will first call load and then return the results.
+        :return: list of Packages offered/received from the providers source.
+        """
+        if not self.is_loaded:
+            logger.debug("Provider not yet loaded. Loading now...")
+            self.load()
+        logger.debug("Loading complete.")
 
     def update(self, package: "Package"):
         """
@@ -37,11 +50,11 @@ class PackageVersion(LooseVersion):
 
 @dataclass(order=True)
 class Package:
-    name: str = field(repr=True)
-    version: PackageVersion = field(repr=True, compare=True)
+    name: "str" = field(repr=True)
+    version: "PackageVersion" = field(repr=True, compare=True)
     catalog: "utils.config.Catalog" = field(repr=True, compare=False)
-    date: datetime = field(repr=False, compare=False)
-    is_autopromote: bool = field(repr=False, compare=False)
+    date: "datetime" = field(repr=False, compare=False)
+    is_autopromote: "bool" = field(repr=False, compare=False)
     is_present: "utils.config.Present" = field(repr=False, compare=False)
     provider: "Provider" = field(repr=False, compare=False)
     jira_id: "str" = field(repr=False, compare=False)
@@ -51,7 +64,7 @@ class Package:
     )
 
     @staticmethod
-    def str_to_version(version_str: str) -> PackageVersion:
+    def str_to_version(version_str: "str") -> "PackageVersion":
         return PackageVersion(version_str)
 
     def update(self):
