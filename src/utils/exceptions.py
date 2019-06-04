90343e42c184b2b7460d9756f00af7f1da23e7ba
Tom Cinbis <tom.cinbis@unibas.ch>
2019-06-04 09:18:21 +0200

    Added new exceptions PromoterException and MunkiItemInMultipleCatalogs

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -1,4 +1,12 @@
-class ProviderDoesNotImplement(Exception):
+class PromoterException(Exception):
+    """This exception class will be extended by all sub exceptions which are special to this project."""
+
+    def __init__(self):
+        super().__init__()
+        self.message = f"{self.__class__} exception occurred."
+
+
+class ProviderDoesNotImplement(PromoterException):
     """This provider does not implement the given method."""
 
     def __init__(self, provider="Provider"):
@@ -10,7 +18,7 @@ class ProviderDoesNotImplement(Exception):
         return self.message
 
 
-class JiraIssueMissingFields(Exception):
+class JiraIssueMissingFields(PromoterException):
     """This Jira issue does not have all required fields for this method."""
 
     def __init__(self):
@@ -20,3 +28,11 @@ class JiraIssueMissingFields(Exception):
 
     def __str__(self):
         return self.message
+
+
+class MunkiItemInMultipleCatalogs(PromoterException):
+    def __init__(self, munki_item):
+        super().__init__()
+        self.provider = "Munki"
+        self.munki_item = munki_item
+        self.message = f"{munki_item.name} in version {munki_item.version} is in more than one catalog. Can't process item. "
