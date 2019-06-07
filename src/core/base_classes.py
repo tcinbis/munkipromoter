03e7c5cd616470d9857a3e7198ea068cacec4453
Tom Cinbis <tom.cinbis@unibas.ch>
2019-06-07 10:13:02 +0200

    Updated annotations and added commit method for dry run option

--- src/core/base_classes.py
+++ src/core/base_classes.py
@@ -1,30 +1,33 @@
+from __future__ import annotations
 from dataclasses import dataclass, field
 from datetime import datetime
 from distutils.version import LooseVersion
-import utils.config
-from utils.config import PackageState
+from typing import List
+
+from utils.config import PackageState, Catalog, JiraAutopromote, Present, JiraLane
 from utils import logger as l
 
 logger = l.get_logger(__file__)
 
 
 class Provider:
-    def __init__(self, name):
+    def __init__(self, name: str, dry_run: bool = False):
         self.name = name
         self.is_loaded = False
         self._packages = list()
+        self._dry_run = dry_run
 
-    def connect(self) -> "bool":
+    def connect(self) -> bool:
         """
         Check whether a connection is already established or try to establish a new one.
         :return: True if the connection was already established or a new one could be created. Otherwise False
         """
         pass
 
-    def load(self) -> "None":
+    def load(self) -> None:
         pass
 
-    def get(self) -> "[Package]":
+    def get(self) -> List[Package]:
         """
         If the providers load method was already called this method will return all received packages in a list.
         Otherwise it will first call load and then return the results.
@@ -36,11 +39,19 @@ class Provider:
         logger.debug("Loading complete.")
         return self._packages
 
-    def update(self, package: "Package"):
+    def update(self, package: Package):
         """
         Updates the information of a package if it already exists or will create a new package.
         All parameters are expected to be passed through **kwargs.
         :return: True if successful or False if not.
+        """
+        pass
+
+    def commit(self) -> bool:
+        """
+        This method finally commits all changes to the providers API in case _dry_run is set to false.
+        Otherwise no changes will be send to the provider.
+        :return: True in case the changes were committed otherwise False
         """
         pass
 
@@ -51,21 +62,19 @@ class PackageVersion(LooseVersion):
 
 @dataclass(order=True)
 class Package:
-    name: "str" = field(repr=True)
-    version: "PackageVersion" = field(repr=True, compare=True)
-    catalog: "utils.config.Catalog" = field(repr=True, compare=False)
-    date: "datetime" = field(repr=False, compare=False)
-    is_autopromote: "utils.config.JiraAutopromote" = field(repr=False, compare=False)
-    is_present: "utils.config.Present" = field(repr=False, compare=False)
-    provider: "Provider" = field(repr=False, compare=False)
-    jira_id: "str" = field(repr=False, compare=False)
-    jira_lane: "utils.config.JiraLane" = field(repr=False, compare=False)
-    state: "utils.config.PackageState" = field(
-        default=PackageState.DEFAULT, repr=False, compare=False
-    )
+    name: str = field(repr=True)
+    version: PackageVersion = field(repr=True, compare=True)
+    catalog: Catalog = field(repr=True, compare=False)
+    date: datetime = field(repr=False, compare=False)
+    is_autopromote: JiraAutopromote = field(repr=False, compare=False)
+    is_present: Present = field(repr=False, compare=False)
+    provider: Provider = field(repr=False, compare=False)
+    jira_id: str = field(repr=False, compare=False)
+    jira_lane: JiraLane = field(repr=False, compare=False)
+    state: PackageState = field(default=PackageState.DEFAULT, repr=False, compare=False)
 
     @staticmethod
-    def str_to_version(version_str: "str") -> "PackageVersion":
+    def str_to_version(version_str: str) -> PackageVersion:
         return PackageVersion(version_str)
 
     def update(self):
