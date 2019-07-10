6cc4e86d14eb5d3a25b7b839a21c1bff8086eb97
koetim01 <tim.koenigl@unibas.ch>
2019-07-10 11:32:37 +0200

    Added the readme documenation and started logger unittest

---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+import unittest
+import pytest
+from utils import logger as log
+
+
+class TestUtils(unittest.TestCase):
+    def test_logger(self):
+        logger = log.get_logger(__file__)
+        assert len(logger.handlers) == 2
+        logger.handlers = list()
+        logger = log.get_logger(__file__, "simple")
+        assert len(logger.handlers) == 2
+        logger.handlers = list()
+        with pytest.raises(ValueError):
+            assert log.get_logger(__file__, "wrong_formatter")
