3a4e7e5b72143f7a048b07047f88eeb518034f85
koetim01 <tim.koenigl@unibas.ch>
2019-07-16 10:56:55 +0200

    Reformating and restructuring

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,14 @@ import os
 import random
 import string
 from datetime import datetime
+from unittest.mock import Mock
 
 import pytest
+from jira.client import ResultList
 from jira.resources import cls_for_resource
 
 from core.base_classes import Package
+from core.promotion import Promoter
 from core.provider.jiraprovider import JiraBoardProvider
 from core.provider.munkiprovider import MunkiRepoProvider
 from utils.config import Catalog, Present, JiraLane, PackageState, JiraAutopromote, conf
@@ -107,4 +110,23 @@ def random_package() -> Package:
         jira_id=get_random_string(9),
         jira_lane=JiraLane(random.choices([e.value for e in JiraLane])[0]),
         state=PackageState(random.choices([e.value for e in PackageState])[0]),
+    )
+
+
+@pytest.fixture
+def set_up_promoter(config, jira_board_provider, munki_repo_provider):
+    jira_board_provider._jira = Mock()
+    jira_board_provider.is_loaded = True
+    jira_issue = [load_jira_test_issue(config.JIRA_DUMP_PATH)]
+    result_list = ResultList(jira_issue, _total=len(jira_issue))
+    jira_board_provider._jira.search_issues.return_value = result_list
+    jira_board_provider.load()
+
+    munki_repo_provider.load()
+    munki_repo_provider._packages_dict = {
+        k: v for k, v in munki_repo_provider._packages_dict.items() if "EN60.8.0" in k
+    }
+
+    return Promoter(
+        munki_repo_provider._packages_dict, jira_board_provider._packages_dict
     )
