38457208fb182a828b9b68801c65515c38a09710
koetim01 <tim.koenigl@unibas.ch>
2019-07-16 11:40:01 +0200

    Test commit for promotion tests

--- tests/test_promotion.py
+++ tests/test_promotion.py
@@ -1,2 +1,20 @@
+from datetime import datetime
+from unittest.mock import Mock
+
+
 class TestPromotion:
-    pass
+    def test_promotion_date(self, config, set_up_promoter):
+        promoter = Mock()
+        promoter.promote.side_effect = set_up_promoter.promote
+
+        promoter.promote()
+        assert (
+                promoter._date_promotions.call_count == 1
+                and promoter._lane_promotions().call_count == 1
+        )
+
+        config.DEFAULT_PROMOTION_DAY = datetime.now().strftime("%A")
+        assert (
+                promoter._date_promotions.call_count == 0
+                and promoter._lane_promotions().call_count == 1
+        )
