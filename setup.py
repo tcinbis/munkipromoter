5b3505f8e40485079b0b97013cf782b6ece9a360
Tom Cinbis <tom.cinbis@unibas.ch>
2019-07-16 14:05:38 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,13 @@ setup(
     name="munki_promoter",
     description="This tool manages Munki packages and utilizes a Jira Kanban board as an user interface.",
     version="2.0",
-    author="Tom Cinbis",
-    author_email="tom.cinbis@unibas.ch",
+    author=[
+        "Tom Cinbis",
+        "Tim Königl",
+        "Client Services Team of the University of Basel IT Services",
+    ],
+    author_email="tom.cinbis@unibas.ch, tim.koenigl@unibas.ch",
+    license="GPLv3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
