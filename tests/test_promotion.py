3d619e93552a8632d78b1bf0f56e7de117597924
Tom Cinbis <tom.cinbis@unibas.ch>
2019-07-17 08:41:50 +0200

    Adding Mock for promotion methods

--- tests/test_promotion.py
+++ tests/test_promotion.py
@@ -1,20 +1,30 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from unittest.mock import Mock
 
 
 class TestPromotion:
     def test_promotion_date(self, config, set_up_promoter):
-        promoter = Mock()
-        promoter.promote.side_effect = set_up_promoter.promote
 
-        promoter.promote()
+        set_up_promoter._date_promotions = Mock()
+        set_up_promoter._lane_promotions = Mock()
+
+        config.DEFAULT_PROMOTION_DAY = (datetime.today() + timedelta(days=1)).strftime(
+            "%A"
+        )
+        set_up_promoter.promote()
+
         assert (
-                promoter._date_promotions.call_count == 1
-                and promoter._lane_promotions().call_count == 1
+            set_up_promoter._date_promotions.call_count == 0
+            and set_up_promoter._lane_promotions.call_count == 1
         )
 
-        config.DEFAULT_PROMOTION_DAY = datetime.now().strftime("%A")
+        set_up_promoter._lane_promotions.reset_mock()
+        set_up_promoter._date_promotions.reset_mock()
+
+        config.DEFAULT_PROMOTION_DAY = datetime.today().strftime("%A")
+        set_up_promoter.promote()
+
         assert (
-                promoter._date_promotions.call_count == 0
-                and promoter._lane_promotions().call_count == 1
+            set_up_promoter._date_promotions.call_count == 1
+            and set_up_promoter._lane_promotions.call_count == 1
         )
