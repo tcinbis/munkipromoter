f2f1c65ec49eec4f40b3395c090b71d9ad710c3c
koetim01 <tim.koenigl@unibas.ch>
2019-07-18 09:01:39 +0200

    Added docu for Promoter

--- src/core/promotion.py
+++ src/core/promotion.py
@@ -17,11 +17,28 @@ logger = log.get_logger(__file__)
 
 
 class Promoter:
+    """
+    The main class for the promotion logic of the program.
+    The munki and jira packages will be compared and the munki packages will be updated according to
+    the state of the jira packages. Additionally automatic catalog transitions are realised if the right
+    criteria are fulfilled.
+    """
+
     def __init__(self, munki_packages: Dict, jira_packages: Dict):
+        """
+        Initializes a promoter object which contains the munki and the jira packages.
+        :param munki_packages: `Dict` the munki packages
+        :param jira_packages: `Dict` the jira packages
+        """
         self.munki_pkgs_dict = munki_packages
         self.jira_pkgs_dict = jira_packages
 
     def promote(self):
+        """
+        The starting point of the promotion. If the day of execution is the same as the configured
+        promotion date the `_date_promotions` are conducted.
+        Afterwards always the `_lane_promotions` are performed.
+        """
         if not datetime.now().strftime("%A") == conf.DEFAULT_PROMOTION_DAY:
             logger.warning(
                 f"Will not promote packages, as it's not {conf.DEFAULT_PROMOTION_DAY}"
@@ -32,6 +49,13 @@ class Promoter:
         self._lane_promotions()
 
     def _lane_promotions(self):
+        """
+        If a jira pacakge is in a promotion lane (TO_*CATALOG*), it is moved to the appropriate
+        catalog (*CATALOG*).
+        Additionally the respective munki package is searched and updated according to the jira
+        package. If the munki package does not exist in the munki_repository it is marked as missing
+        in jira.
+        """
         for jira_pkg in self.jira_pkgs_dict.values():
             if jira_pkg.jira_lane.is_promotion_lane:
                 # Pkg is in a promotion lane
@@ -71,6 +95,10 @@ class Promoter:
             self.jira_pkgs_dict.update({jira_pkg.key: jira_pkg})
 
     def _date_promotions(self):
+        """
+        If a package is tagged as autopromote and is longer than one week in its catalog it is moved
+        to the next catalog. The jira package is updated.
+        """
         # Start to check for promotion as it is the correct weekday
         for jira_pkg in self.jira_pkgs_dict.values():
             if (
